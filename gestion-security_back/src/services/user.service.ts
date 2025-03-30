69340c7f1afd1d99ab7dc1fcd245fb27f81e860c
andrea00728 <andrea112samuel@gmail.com>
2025-03-31 00:28:42 +0300

    mis a jour du service

--- gestion-security_back/src/services/user.service.ts
+++ gestion-security_back/src/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import  bcrypt from 'bcrypt';
+import *as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '../entities/user.entity';
 import { registerDto } from '../dto/userDTO/register.dto';
@@ -15,9 +15,12 @@ export class UserService {
     private readonly jwtService: JwtService,
   ) {}
 
+
   /**
      * @property creation de 'utilisateur ,verification  d'un utilisateur deja exister avant de la creation
      */
+
+
   async register(registerDto: registerDto): Promise<{ message: string }> {
     const { username, email, password } = registerDto;
     const existingUser = await this.userRepository.findOne({ where: [{ email }, { username }] });
@@ -32,6 +35,7 @@ export class UserService {
     return { message: 'Utilisateur enregistré avec succès' };
   }
 
+
   /**
    * 
    * @param loginDto 
@@ -39,6 +43,7 @@ export class UserService {
    * @property connexion de avec le compte existant de l'utilisateur enregistrer
    */
   
+
   async login(loginDto: LoginDto): Promise<{ access_token: string }> {
     const { email, password } = loginDto;
     const user = await this.userRepository.findOne({ where: { email } });
