b5c3967a50061a36afa7b7e2d6a8b67fc819d2eb
andrea00728 <andrea112samuel@gmail.com>
2025-03-30 20:24:07 +0300

    ajout de contextProvider

---
+++ GestionSecuruty/src/context/ContextProvider.tsx
@@ -0,0 +1,51 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+
+interface StateContextType {
+    user:any|null;
+    token:string|null;
+    setUser:(any:null)=>void;
+    setToken:(string:null)=>void;
+}
+
+const StateContext=createContext<StateContextType>({
+    user:null,
+    token:null,
+    setUser:()=>{},
+    setToken:()=>{},
+});
+
+interface ContextProviderProps{
+    children:ReactNode;
+}
+
+export const ContextProvider=({children}:ContextProviderProps)=>{
+    const [user,setUser]=useState<any|null>(null);
+    const [token,_setToken]=useState<string|null>(()=>sessionStorage.getItem("ACCESS_TOKEN")||null);
+
+    useEffect(()=>{
+        console.log("token recupere par le COntextProvider:",token);
+        if(!token){
+            _setToken(null);
+            sessionStorage.removeItem("ACCESS_TOKEN");
+        }
+    },[token]);
+
+    const setToken=(token:string|null)=>{
+        _setToken(token);
+        if(token){
+            sessionStorage.setItem("ACCESS_TOKEN",token);        
+        }else{
+            sessionStorage.removeItem("ACCESS_TOKEN");
+        }
+    };
+
+    return (
+        <StateContext.Provider  value={{user,token,setToken,setUser}}>
+            {children}
+        </StateContext.Provider>
+    );
+};
+
+export const useStateContext=()=>{
+    return useContext(StateContext);
+}
