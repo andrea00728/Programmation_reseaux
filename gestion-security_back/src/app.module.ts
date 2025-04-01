a2167242b6bfb5fb8befebe2138ecb123788e1fe
andrea00728 <andrea112samuel@gmail.com>
2025-04-01 03:16:51 +0300

    ajout entity Pricing dans app.model ts

--- gestion-security_back/src/app.module.ts
+++ gestion-security_back/src/app.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user.module';
+import { Pricing } from './entities/pricing.entity';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -20,7 +21,7 @@ import { UserModule } from './modules/user.module';
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_DATABASE'),
-        entities: [User],
+        entities: [User,Pricing],
         synchronize: true,
       }),
       inject: [ConfigService],
